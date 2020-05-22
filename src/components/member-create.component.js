e69a7f321c05c2c1380bb7afd042119b77999da8
Muriel Proton <muriel.proton@epitech.eu>
2020-05-22 12:39:04 +0200

    TODO Lister tous les key words de tous les posts

--- src/components/member-create.component.js
+++ src/components/member-create.component.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import axios from 'axios';
 import { Redirect } from "react-router-dom";
-//import { Redirect } from "react-router";
-
 
 export default class CreateMember extends Component {
 
---
+++ src/components/post-search-key-word.component.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import axios from 'axios';
+import { Redirect } from "react-router-dom";
+
+export default class ListOfPostsByKeyWords extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      showMenu: false,
+    };
+    
+    this.showMenu = this.showMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+  
+  showMenu(event) {
+    //event.preventDefault();
+    
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener('click', this.closeMenu);
+    });
+  }
+  
+  closeMenu(event) {
+    
+    if (!this.dropdownMenu.contains(event.target)) {
+      
+      this.setState({ showMenu: false }, () => {
+        document.removeEventListener('click', this.closeMenu);
+      });  
+      
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.showMenu}>
+            Choose a Keyword
+        </button>
+        
+        {
+          this.state.showMenu
+            ? (
+              <div
+                className="menu"
+                ref={(element) => {
+                  this.dropdownMenu = element;
+                }}
+              >
+                <button> # 1 </button>
+                <button> # 2 </button>
+                <button> # 3 </button>
+              </div>
+            )
+            : (
+              null
+            )
+        }
+      </div>
+    );
+  }
+}
